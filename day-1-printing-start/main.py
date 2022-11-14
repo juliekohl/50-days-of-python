637f19ac2e30f1350f9389d61ef46b71860a0493
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-14 14:38:01 -0300

    first commitD

---
+++ day-1-printing-start/main.py
@@ -0,0 +1,45 @@
+# Write your code below this line
+# print("Hello Julie!")
+
+# Printing String
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# Printing String with debug
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# Prints the number of char in my name use prompt
+# print(len(input("What is your name? ")))
+
+# variables
+# input("What is your name? ")
+#
+# name = "Julie"
+# print(name)
+#
+# name = "Cesar"
+# print(name)
+#
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+# a = input("a: ")
+# b = input("b: ")
+#
+# inputA = b
+# inputB = a
+#
+# print("a: " + inputA)
+# print("b: " + inputB)
+
+# Project Band Name Generator
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+
+print("Your band name could be " + city_name + " " + pet_name)
