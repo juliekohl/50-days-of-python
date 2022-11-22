4dd7d146363f13968cd2f1fa87932f940900e46e
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-22 15:21:30 -0300

    Exercise Leap Year.

--- day-3-conditional/main.py
+++ day-3-conditional/main.py
@@ -34,18 +34,31 @@
 #     print("Sorry, you have to grow taller before you can ride.")
 
 # Exercises BMI
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+#
+# bmi = round(weight / height ** 2)
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi < 25:
+#     print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi < 30:
+#     print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi < 35:
+#     print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese.")
 
-bmi = round(weight / height ** 2)
-if bmi < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi < 25:
-    print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi < 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi < 35:
-    print(f"Your BMI is {bmi}, you are obese.")
+# Exercise Leap Year
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
 else:
-    print(f"Your BMI is {bmi}, you are clinically obese.")
-
+    print("Not leap year")
