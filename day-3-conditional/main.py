1665fa14ed1fd20dc86cc88e97788b7a2ef1e03d
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-22 14:32:35 -0300

    Exercise BMI.

--- day-3-conditional/main.py
+++ day-3-conditional/main.py
@@ -18,19 +18,34 @@
 #     print("This is an odd number.")
 
 # Nested if, elif, else
-print("Welcome to the rollercoaster!")
-height = int(input("what is your height in cm? "))
-
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")
-    else:
-        print("Please pay $12.")
-else:
-    print("Sorry, you have to grow taller before you can ride.")
-
+# print("Welcome to the rollercoaster!")
+# height = int(input("what is your height in cm? "))
 #
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# Exercises BMI
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
