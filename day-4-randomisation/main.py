eaf814c0456d1047b909aec27d65bdea2fde266d
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-23 16:44:08 -0300

    Heads or Tails.

--- day-4-randomisation/main.py
+++ day-4-randomisation/main.py
@@ -1,10 +1,18 @@
 import random
+# Random mModules
+#
+# randomIntegeter = random.randint(1, 10)
+# print(randomIntegeter)
+#
+# randomFloat = random.random() * 5
+# print(randomFloat)
+#
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}")
 
-randomIntegeter = random.randint(1, 10)
-print(randomIntegeter)
-
-randomFloat = random.random() * 5
-print(randomFloat)
-
-love_score = random.randint(1, 100)
-print(f"Your love score is {love_score}")
+# Exercise Heads or Tails
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
