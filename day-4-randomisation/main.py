3a7c75284485a17899c5373ad5410b43759b3f3f
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-23 17:13:59 -0300

    Lists.append() and .extend().

--- day-4-randomisation/main.py
+++ day-4-randomisation/main.py
@@ -1,4 +1,4 @@
-import random
+# import random
 # Random mModules
 #
 # randomIntegeter = random.randint(1, 10)
@@ -11,8 +11,22 @@ import random
 # print(f"Your love score is {love_score}")
 
 # Exercise Heads or Tails
-random_side = random.randint(0, 1)
-if random_side == 1:
-  print("Heads")
-else:
-  print("Tails")
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#   print("Heads")
+# else:
+#   print("Tails")
+
+# Offset and appending items to lists
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america)
+print(states_of_america[3]) # Georgia
+
+states_of_america[3] = "Georgya"
+print(states_of_america[3]) # Georgya
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+states_of_america.extend(dirty_dozen)
+print(states_of_america)
