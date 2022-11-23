549166cd917a63d5ac769fd20990a158fde09ba2
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-23 09:33:44 -0300

    Project 3 treasure island.

--- day-3-conditional/main.py
+++ day-3-conditional/main.py
@@ -117,30 +117,103 @@
 # print(f"Your final bill is: ${bill}.")
 #
 # Logical Operators
-print("Welcome to the rollercoaster!")
-height = int(input("what is your height in cm? "))
-bill = 0
+# print("Welcome to the rollercoaster!")
+# height = int(input("what is your height in cm? "))
+# bill = 0
+#
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     elif age >= 45 and age <= 55:
+#         print("Everything is going to be ok. Have a free ride on us!")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+#
+#     wants_photo = input("Do you want a phot taken? Y to Yes or N to no. ")
+#     if wants_photo == "Y":
+#         bill += 3
+#
+#     print(f"Your final bill is {bill}")
+#
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+#
+# Love Calculator Exercise
+#
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+#
+# combined_names = name1 + name2
+# lower_names = combined_names.lower()
+# t = lower_names.count("t")
+# r = lower_names.count("r")
+# u = lower_names.count("u")
+# e = lower_names.count("e")
+# first_digit = t + r + u + e
+#
+# l = lower_names.count("l")
+# o = lower_names.count("o")
+# v = lower_names.count("v")
+# e = lower_names.count("e")
+# second_digit = l + o + v + e
+#
+# score = int(str(first_digit) + str(second_digit))
+#
+# if (score < 10) or (score > 90):
+#   print(f"Your score is {score}, you go together like coke and mentos.")
+# elif (score >= 40) and (score <= 50):
+#   print(f"Your score is {score}, you are alright together.")
+# else:
+#   print(f"Your score is {score}.")
+#
+# Project 3 - Treasure Island
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        bill = 5
-        print("Child tickets are $5.")
-    elif age <= 18:
-        bill = 7
-        print("Youth tickets are $7.")
-    elif age >= 45 and age <= 55:
-        print("Everything is going to be ok. Have a free ride on us!")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
     else:
-        bill = 12
-        print("Adult tickets are $12.")
-
-    wants_photo = input("Do you want a phot taken? Y to Yes or N to no. ")
-    if wants_photo == "Y":
-        bill += 3
-
-    print(f"Your final bill is {bill}")
-
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
 else:
-    print("Sorry, you have to grow taller before you can ride.")
+  print("You fell into a hole. Game Over.")
