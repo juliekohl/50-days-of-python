b8b3c5c797d591b1539ab18b48bd714f653ba453
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 14:12:58 -0300

    Exercise high score.

--- day-5-loop/main.py
+++ day-5-loop/main.py
@@ -8,17 +8,34 @@
 # print(fruits)
 
 # Average Height
-student_heights = input("Input a list of student heights ").split()
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_scores)):
+#       student_scores[n] = int(student_scores[n])
+# print(student_scores)
+#
+# total_height = 0
+# for height in student_heights:
+#     total_height += int(height)
+# print(f"total height = {total_height}")
+#
+# number_of_students = 0
+# for student in student_heights:
+#     number_of_students += 1
+# print(f"number of students = {number_of_students}")
+#
+# average_height = round(total_height / number_of_students)
+# print(average_height)
 
-total_height = 0
-for height in student_heights:
-    total_height += int(height)
-print(f"total height = {total_height}")
+# High Score
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
 
-number_of_students = 0
-for student in student_heights:
-    number_of_students += 1
-print(f"number of students = {number_of_students}")
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+        # print(highest_score)
 
-average_height = round(total_height / number_of_students)
-print(average_height)
+print(f"The highest score in the class is: {highest_score}")
