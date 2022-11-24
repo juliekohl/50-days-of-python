d73a37f36ee1921849e37477ce91f93dac68079c
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 14:26:21 -0300

    Exercise even numbers.

--- day-5-loop/main.py
+++ day-5-loop/main.py
@@ -50,7 +50,22 @@
 # for number in range(1, 11, 3):
 #     print(number) # print 1 4 7 10
 
-total = 0
-for number in range(1, 101):
-    total += number
-print(total) # print 5050
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total) # print 5050
+
+# Add even numbers
+even_sum = 0
+for number in range(2, 101, 2):
+    even_sum += number
+print(even_sum) # 2550
+
+# or
+
+# alternative_sum = 0
+# for number in range(1, 101):
+#   if number % 2 == 0:
+#     # print(number)
+#     alternative_sum += number
+# print(alternative_sum) # 2550
