109ef9ebc48e4e7f27b8d3038d3bcfab62689b58
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 09:07:26 -0300

    Exercise Banker Roulette.

--- day-4-randomisation/main.py
+++ day-4-randomisation/main.py
@@ -1,5 +1,5 @@
 # import random
-# Random mModules
+# Random Modules
 #
 # randomIntegeter = random.randint(1, 10)
 # print(randomIntegeter)
@@ -18,15 +18,31 @@
 #   print("Tails")
 
 # Offset and appending items to lists
-states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+#
+# print(states_of_america)
+# print(states_of_america[3]) # Georgia
+#
+# states_of_america[3] = "Georgya"
+# print(states_of_america[3]) # Georgya
+#
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+#
+# states_of_america.extend(dirty_dozen)
+# print(states_of_america)
 
-print(states_of_america)
-print(states_of_america[3]) # Georgia
+# Exercise Banker Roulette
+import random
 
-states_of_america[3] = "Georgya"
-print(states_of_america[3]) # Georgya
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
 
-dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
 
-states_of_america.extend(dirty_dozen)
-print(states_of_america)
+print(person_who_will_pay + " is going to buy the meal today!")
