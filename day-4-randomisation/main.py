23a30f676a6de1c5690eb1aa2f003b44aadbc0b5
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 11:26:06 -0300

    Exercise treasure map.

--- day-4-randomisation/main.py
+++ day-4-randomisation/main.py
@@ -76,4 +76,19 @@
 # print(dirty_dozen[1]) # vegetables only
 # print(dirty_dozen[1][2]) # Tomatoes
 # print(dirty_dozen[1][3]) # Celery
-#
+
+# Treasure Map
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? ")
+
+row = int(position[0])
+col = int(position[1])
+
+map[col - 1][row - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
