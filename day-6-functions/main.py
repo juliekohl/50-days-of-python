2459cd65b1ffa27f6f97781b6d65ba95e472c3ff
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-25 14:35:05 -0300

    While loops.

--- day-6-functions/main.py
+++ day-6-functions/main.py
@@ -5,13 +5,37 @@
 #
 # my_function()
 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# jump()
+#
+# While Loop
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+
+# Hurdles with while loops
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
 def jump():
-    move()
     turn_left()
     move()
     turn_right()
@@ -20,11 +44,8 @@ def jump():
     move()
     turn_left()
 
-jump()
-
-# While Loop
-number_of_hurdles = 6
-while number_of_hurdles > 0:
-    jump()
-    number_of_hurdles -= 1
-    print(number_of_hurdles)
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
