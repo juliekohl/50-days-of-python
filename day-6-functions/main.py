d88605e6a79893419e7d8bc73949f7de414b9bd1
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-25 14:22:57 -0300

    While loop.

--- day-6-functions/main.py
+++ day-6-functions/main.py
@@ -1,6 +1,30 @@
 # Functions
-def my_function():
-    print("Hello")
-    print("Bye")
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+#
+# my_function()
 
-my_function()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+jump()
+
+# While Loop
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
