0b02f9364e8ef6703f24fc3d7ea19ed10200058c
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-12 10:47:44 -0300

    Modify a global variable.

--- day-12-globa-scope/main.py
+++ day-12-globa-scope/main.py
@@ -6,10 +6,10 @@ from typing import List
 #
 # def increase_enemies() -> None:
 #   enemies = 2
-#   print(f"enemies inside function: {enemies}")
+#   print(f"enemies inside function: {enemies}") # 2
 #
 # increase_enemies()
-# print(f"enemies outside function: {enemies}")
+# print(f"enemies outside function: {enemies}") # 1
 
 # Local Scope
 # def drink_potion() -> None:
@@ -30,15 +30,28 @@ from typing import List
 # print(player_health) # 10
 
 # There is no Block Scope
-game_level: int = 3
-
-
-def create_enemy() -> None:
-    enemies: List[str] = ["Skeleton", "Zombie", "Alien"]
-
-    if game_level < 5:
-        new_enemy: str = enemies[0]
-    print(new_enemy)
+# game_level: int = 3
+#
+#
+# def create_enemy() -> None:
+#     enemies: List[str] = ["Skeleton", "Zombie", "Alien"]
+#
+#     if game_level < 5:
+#         new_enemy: str = enemies[0]
+#     print(new_enemy)
 
 
 # print(new_enemy) # error var is not defined
+
+# Modify a Global Variable
+enemies: int = 1
+
+
+def increase_enemies() -> int:
+    print(f"enemies inside function: {enemies}")  # 1
+    return enemies + 1
+
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")  # 2
+
