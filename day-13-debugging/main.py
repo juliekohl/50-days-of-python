83ba20269324ed9d62c3f5b2387a08c9ac5581b0
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-13 10:20:38 -0300

    Debugging FrizzBuzz.

--- day-13-debugging/main.py
+++ day-13-debugging/main.py
@@ -58,16 +58,26 @@ from typing import List
 #     print("This is an odd number.")
 
 # Debugging Leap Year
-year: int = int(input("Which year do you want to check? "))
+# year: int = int(input("Which year do you want to check? "))
+#
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year.")
-        else:
-            print("Not leap year.")
+# Debugging FizzBuzz
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
     else:
-        print("Leap year.")
-else:
-    print("Not leap year.")
-
+        print(number)
