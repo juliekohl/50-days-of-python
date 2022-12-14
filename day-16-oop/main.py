74710c8442badee99fd126ced90fd01d177dddc0
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-14 10:51:52 -0300

    Modifying Object Attributes and calling methods.

--- day-16-oop/main.py
+++ day-16-oop/main.py
@@ -1,12 +1,22 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
+from prettytable import PrettyTable
+table = PrettyTable()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# table.field_names = ["Pokemon Name", "Type"]
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
 
