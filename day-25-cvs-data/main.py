43d0c4ec200d4c7abfaf915b76773a4dfdd735a9
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-17 10:15:21 -0300

    Squirrel Count CSV.

--- day-25-cvs-data/main.py
+++ day-25-cvs-data/main.py
@@ -56,3 +56,21 @@ import pandas
 # }
 # data = pandas.DataFrame(data_dict)
 # data.to_csv("new-data.csv") # this will create new file with the data_dict inside!
+
+# Squirrel Count csv
+data = pandas.read_csv("2018-central-park-squirrel-census-squirrel-data.csv")
+grey_squirrels_count: float = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count: float = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count: float = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrels_count) # 2473
+print(red_squirrels_count) # 392
+print(black_squirrels_count) # 103
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count],
+}
+
+dataframe = pandas.DataFrame(data_dict)
+dataframe.to_csv("squirrel_count.csv")
