a8e2acb6ec386d305adefa82b1dae946a1d56e8a
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-17 17:55:06 -0300

    Improvement.

--- day-26-list-comprehension/main.py
+++ day-26-list-comprehension/main.py
@@ -90,6 +90,12 @@ import pandas
 #
 
 # Dictionary Comprehension
+# import random
+
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# students_scores = {student: random.randint(1, 100) for student in names}
+# passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
+
 # student_dict = {
 #     "students": ["Julie", "Angie", "Kelly"],
 #     "scores": [87, 85, 88],
