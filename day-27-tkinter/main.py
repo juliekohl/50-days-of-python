a83c2b2fbf0c8678a3ec4af4fde583e56c2158c4
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-19 09:48:55 -0300

    Create button, entry and component options.

--- day-27-tkinter/main.py
+++ day-27-tkinter/main.py
@@ -1,12 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "My new Label Text"
+my_label.config(text="My new Label Text")
+
+
+# Button
+def button_clicker() -> None:
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicker)
+button.pack()
+
+# Entry - input
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 
 window.mainloop()
