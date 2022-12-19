c74d1a916ddd389592fa659c01054a934014f560
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-19 09:09:21 -0300

    Create **kwargs.

--- day-27-tkinter/playground.py
+++ day-27-tkinter/playground.py
@@ -1,11 +1,26 @@
-def add(*args: int) -> int:
-    # print(type(args)) # class 'tuple'
-    # print(args[0]) # 3
-    # print(args[1]) # 5
-    sum: int = 0
-    for n in args:
-        sum += n
-    return sum
+# *args
+# def add(*args: int) -> int:
+#     # print(type(args)) # class 'tuple'
+#     # print(args[0]) # 3
+#     # print(args[1]) # 5
+#     sum: int = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+#
+# print(add(3, 5, 6, 2, 1, 7, 4, 3)) # 31
+
+from typing import Dict, Any
 
 
-print(add(3, 5, 6, 2, 1, 7, 4, 3)) # 31
+# **kwargs
+def calculate(n: int, **kwargs: Dict[str, Any]) -> None:
+    # print(type(kwargs)) # class 'dict'
+    print(kwargs) # {'add': 3, 'multiply': 5}
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n) # 25
+
+
+calculate(2, add=3, multiply=5)
