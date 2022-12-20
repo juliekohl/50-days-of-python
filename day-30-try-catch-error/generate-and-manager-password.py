56ffbccc58ab84858688279dedec6051629dfeac
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-20 17:59:11 -0300

    Create search button to find password.

--- day-30-try-catch-error/generate-and-manager-password.py
+++ day-30-try-catch-error/generate-and-manager-password.py
@@ -100,8 +100,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -110,6 +110,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
